fd36a2e2833f0896e99b58445162451f24685a2c
jainsa88 <sachin.jain1@ltm.com>
2026-05-05 11:14:34 +0000

    Add populate_db Django management command

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,4 +1,3 @@
-
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Workout
 
@@ -7,4 +6,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         # Create test data objects here
+        user = User.objects.create(username="testuser", email="testuser@example.com")
+        team = Team.objects.create(name="Test Team")
+        activity = Activity.objects.create(name="Test Activity")
+        workout = Workout.objects.create(user=user, team=team, activity=activity)
         self.stdout.write(self.style.SUCCESS("Database populated successfully"))
